607c9d4f34d466fd4cd39df6e3721f8e6f6f8a91
mxq7854687 <mxq7854687@gmail.com>
2022-05-06 22:10:55 +0800

    test should success

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -21,7 +21,6 @@ func TestMain(m *testing.M) {
 	if err != nil {
 		log.Fatal("DB Connection [ Failed ]: ", err)
 	}
-	log.Fatal("DB Connection [ Success ]: ", err)
 
 	testQueries = New(connection)
 
