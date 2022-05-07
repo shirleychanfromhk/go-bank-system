aa4161813c81341d75ddf67abe0a9d8b3772b5ed
Tsz Yin <cheung@Tszs-MacBook-Pro.local>
2022-05-08 00:04:31 +0100

    Modify sql.db initialisation

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -19,7 +19,7 @@ var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 	var err error
-	testDB, err := sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("DB Connection [ Failed ]: ", err)
 	}
