6d9e97384019c018b758487842c981bf84443804
shirleychanfromhk <71444040+shirleychanfromhk@users.noreply.github.com>
2022-06-08 21:58:51 +0100

    BK-5333 add unit test

---
+++ api/exchange_test.go
@@ -0,0 +1,64 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	mockdb "simplebank/db/mock"
+	"simplebank/db/util"
+	"strconv"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetExchangeRate(t *testing.T) {
+	mockToCurrency := util.HKD
+	mockFromCurrency := util.USD
+	mockAmount := strconv.FormatInt(util.RandomInt(1, 1000), 10)
+
+	testCases := []struct {
+		name          string
+		body          gin.H
+		checkResponse func(recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			body: gin.H{
+				"to":     mockToCurrency,
+				"from":   mockFromCurrency,
+				"amount": mockAmount,
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+			},
+		},
+		//TODO design a test case for bad request
+	}
+
+	for i := range testCases {
+		testCase := testCases[i]
+		t.Run(testCase.name, func(t *testing.T) {
+			controller := gomock.NewController(t)
+			defer controller.Finish()
+
+			store := mockdb.NewMockStore(controller)
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
+
+			res, err := json.Marshal(testCase.body)
+			require.NoError(t, err)
+
+			exchangeURL := "/exchange"
+			request, err := http.NewRequest(http.MethodGet, exchangeURL, bytes.NewReader(res))
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, request)
+			testCase.checkResponse(recorder)
+		})
+	}
+
+}
